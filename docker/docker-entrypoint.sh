e3b5d151999197545d49933e3a694f478492a64c
wang <470750572@qq.com>
2021-01-17 11:34:01 +0800

    修改为自己仓库

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,7 +6,7 @@ echo -e "\n========================1. 更新源代码========================\n"
 # WhichDep=$(grep "/jd-base" "${JD_DIR}/.git/config")
 # echo -e "\n========================2. 更新源代码========================\n"
 # if [[ ${WhichDep} == *github* ]]; then
-ScriptsURL=https://github.com/LXK9301/jd_scripts
+ScriptsURL=https://github.com/Kkwoai/my_jd_scripts
 ShellURL=https://github.com/Kkwoai/my_jd_base
 # else
 #   ScriptsURL=https://gitee.com/lxk0301/jd_scripts
